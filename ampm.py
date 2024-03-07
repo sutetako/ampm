f14b7faf06757a789c304f8ed076678b4cacb9cb
Moto Endo <endo.moto.biz@gmail.com>
2024-03-08 00:12:43 +0900

    add memory tracking (#11)

--- ampm.py
+++ ampm.py
@@ -33,7 +33,7 @@ class CPUTime:
 
 def read_stat(pid: int) -> CPUTime:
     with open(os.path.join('/proc', str(pid), 'stat'), 'r') as f:
-        s = f.read().split(' ')
+        s = f.read().split()
     # comm, utime, stime, cutime, cstime, num_threads
     return CPUTime(int(s[13]), int(s[14]), int(s[15]), int(s[16]),
                    int(s[19]))
@@ -43,6 +43,13 @@ def read_comm(pid: int) -> str:
     with open(os.path.join('/proc', str(pid), 'cmdline'), 'r') as f:
         cmdline = f.read().split('\0')
     return cmdline[0]
+
+
+def read_smaps(pid: int) -> int:
+    with open(os.path.join('/proc', str(pid), 'smaps_rollup'), 'r') as f:
+        next(f)
+        rss = f.readline().split()[1]
+    return int(rss)
 
 
 def run(pid: int, interval: float, duration: float, output_type: str):
@@ -59,15 +66,19 @@ def run(pid: int, interval: float, duration: float, output_type: str):
     prev_cpu = b_cpu
 
     try:
+        # header
+        print(f'Command{sep}CPUUsage[%]{sep}RSS[kB]', flush=True)
         while duration > 0:
             time.sleep(interval - (time.perf_counter() - t))
 
             t = time.perf_counter()
             diff_cpu = read_stat(pid)
-            usage = prev_cpu.usage(interval, diff_cpu)
+            cpu_usage = prev_cpu.usage(interval, diff_cpu)
             prev_cpu = diff_cpu
 
-            print(f'{comm}{sep}{usage}', flush=True)
+            rss = read_smaps(pid)
+
+            print(f'{comm}{sep}{cpu_usage}{sep}{rss}', flush=True)
             duration = duration - interval
     except KeyboardInterrupt:
         pass
@@ -81,12 +92,11 @@ if __name__ == '__main__':
     parser.add_argument("pid", help="process ID. e.g. $(pidof foo)", type=int)
 
     parser.add_argument("-r", "--rate",
-                        help="calculation frequency of CPU usage [Hz].\
-                        default:1 [0.01<]",
+                        help="calculation frequency. default:1 (>= 0.01)",
                         type=float, default="1", action="store")
     parser.add_argument("-d", "--duration",
                         help="monitoring duration [sec], 0 means inf.\
-                        default:0 [0=<]",
+                        default:0 (>= 0)",
                         type=int, default=0, action="store")
     parser.add_argument("-t", "--type",
                         help="Output type. default:"" [|csv]",
